35a73891335fc64bf1dcb3faa353c95064235d78
BiswajitThakur <biswajit733127@gmail.com>
2025-02-25 08:42:57 +0530

    into raw mode

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::io::{self, Read};
 
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+
 fn main() {
+    enable_raw_mode().unwrap();
     for b in io::stdin().bytes() {
         let c = b.unwrap() as char;
         if c == 'q' {
@@ -8,4 +11,5 @@ fn main() {
         }
         println!("> {}", c);
     }
+    disable_raw_mode().unwrap();
 }
